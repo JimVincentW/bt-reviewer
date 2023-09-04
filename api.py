9f28bd1e42b734bea896674bd3d677134e5dfd0f
Jim Wagner <jimvwagner@googlemail.com>
2023-09-05 01:43:41 +0200

    v4

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import api_main as main  # import your refactored main script
+from api_main import DocumentHandler
 import logging
 
 app = Flask(__name__)
@@ -17,13 +18,19 @@ def receive_url():
         if not url:
             return jsonify({"error": "No URL provided"}), 400
         
-        # Call your refactored main function that processes a URL
-        results = main.process_url(url)
+        # Assume `main.process_url(url)` scrapes the document's URL and returns the document URL and date.
+        # This part may vary depending on how your `main.process_url(url)` is structured.
+        doc_url, date = main.process_url(url)
+        
+        # Download the document using DocumentHandler
+        downloaded_file_path = DocumentHandler.download_file(doc_url, date)
+        
+        # If you need to further process the document, you can call the `process_documents` method.
+        # This assumes that you want to process all documents within the "Drucksachen" directory.
+        results = DocumentHandler.process_documents()
+        
     except Exception as e:
         logger.exception("Failed to process URL.")  # This will log the full exception traceback
         return jsonify({"error": str(e)}), 500
     
     return jsonify({"status": "success", "data": results}), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
