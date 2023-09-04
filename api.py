ed058588e18428cfc998ca2d0ac3a9419ec195e8
Jim Wagner <jimvwagner@googlemail.com>
2023-09-04 22:19:36 +0200

    updated docker build

--- api.py
+++ api.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 
 @app.route('/receive-url', methods=['POST'])
 def receive_url():
-    data = request.json
-    url = data.get('url')
-    
-    if not url:
-        return jsonify({"error": "No URL provided"}), 400
-    
-    # Call your refactored main function that processes a URL
-    results = main.process_url(url)
+    try:
+        data = request.json
+        url = data.get('url')
+        
+        if not url:
+            return jsonify({"error": "No URL provided"}), 400
+        
+        # Call your refactored main function that processes a URL
+        results = main.process_url(url)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
     return jsonify(results), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001)
